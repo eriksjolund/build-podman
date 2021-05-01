fcf681d0b6745d6a0f0a0b4c04209f7e7fc501e9
Erik Sjölund <erik.sjolund@gmail.com>
2021-05-01 17:08:21 +0200

    Add build_container.bash
    Signed-off-by: Erik Sjölund <erik.sjolund@gmail.com>

---
+++ build_container.bash
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+if [[ $# -ne 1 ]]; then
+    echo "Error: Wrong number of parameters"
+    exit 1
+fi
+
+config="$1"
+
+buildargs=($(cat checkout_build_podman/config/${config}.json | jq -r  '[.container["build-args"] | to_entries[] | @sh  "--build-arg=\(.key)=\(.value)"] | join(" ") '))
+
+dockerfile="$(cat checkout_build_podman/config/${config}.json | jq -r .container.dockerfile )"
+
+podman --storage-driver vfs build ${buildargs[@]} -t "build-podman:$config" -f "checkout_build_podman/container/$dockerfile" checkout_build_podman/
